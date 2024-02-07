7e263ff62eb8a1f5171b6d60b7a4a2e33d9d5421
blackadder-git <sauterelle.web@gmail.com>
2024-02-07 14:09:50 +0000

    added contacts service

---
+++ src/app/contacts/contact.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Contact } from './contact.model';
+import { MOCKCONTACTS } from './MOCKCONTACTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  contacts: Contact[] = [];
+
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+  }
+
+  getContacts() {
+    return this.contacts.slice();
+  }
+
+  // Return contact object matching id or, if not found, return null
+  getContact(id: String) {
+    this.contacts.forEach(contact => {
+      if (contact.id == id) {
+        return contact
+      }
+    });
+
+    return null
+  }
+}
